052835ac0d96cd5a63c22a8ea37e6060bff790c4
beatrice_yap_sm <beatrice_yap_sm@aiap.sg>
2023-10-17 12:53:45 +0800

    test1

--- testing_only2.py
+++ testing_only2.py
@@ -4,6 +4,7 @@ import streamlit as st
 from audio_to_text import audio_to_text, audio_to_text_our_model
 from st_audiorec import st_audiorec
 from scipy.io import wavfile
+import io
 
 st.set_page_config(page_title="My webpage", page_icon=":tada:",
                    layout="wide")
@@ -13,12 +14,21 @@ st.title("Singaporean English Speech to Text")
 
 left_column, right_column = st.columns(2)
 
+with right_column:
+    st.markdown(
+        f"""
+        <img src="https://mustsharenews.com/wp-content/uploads/2019/09/pck-yellow-boots.jpg" width="275" height="600">
+        """,
+        unsafe_allow_html=True,
+    )
+
 with left_column:
     st.subheader("Record an audio:")
     audio_path = "audio.wav"  # Define the path to save the audio file in the current working directory
     audio_recording = st_audiorec() #"Click to record", "Click to stop recording")
     if audio_recording is not None:
         # st.audio(audio_recording, format='audio/wav') 
+        # st.audio(audio_recording, format='audio/wav')
 
     # if len(audio_recording) > 0:
     #     st.audio(audio_recording.export().read())
@@ -27,7 +37,8 @@ with left_column:
         try:
             # Open and process the audio file
             # audio_recording.export(audio_path, format="wav")  # Save the recorded audio
-            audio_data, sample_rate = librosa.load(audio_recording, sr=None) #audio_path
+            # audio_data, sample_rate = librosa.load(audio_recording, sr=None) #audio_path
+            audio_data, sample_rate = librosa.core.load(io.BytesIO(audio_recording), sr=16000)
             text = audio_to_text()
             st.subheader("Baseline Transcription:")
             st.write(text)
@@ -43,10 +54,4 @@ with left_column:
             st.error(f"Error (Transcription): {str(e)}")
 
 
-with right_column:
-    st.markdown(
-        f"""
-        <img src="https://mustsharenews.com/wp-content/uploads/2019/09/pck-yellow-boots.jpg" width="275" height="600">
-        """,
-        unsafe_allow_html=True,
-    )
+
